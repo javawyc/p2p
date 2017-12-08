6e723455382a8ced94a3ec08b392e8e13c76a592
CatGod <32323146+CatGodwyc@users.noreply.github.com>
2017-12-08 20:09:06 +0800

    1.0 主体

---
+++ sso/src/main/java/com/p2p/sso/SsoApplication.java
@@ -0,0 +1,12 @@
+package com.p2p.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoApplication.class, args);
+	}
+}
